5ea1234f6b504ffb55b24e6cd8b2ece705ab27ac
Vlad <dv.vlad98@mail.ru>
2017-06-06 02:27:04 +0300

    Lab3 Angular4 REST service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { BookComponent } from './book/book.component';
+import { BookItemComponent } from './book/book-item/book-item.component';
+import { BookFormComponent } from './book/book-form/book-form.component';
+import {BookService} from "./shared/book.service";
+import { BookDetailsComponent } from './book/book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookComponent,
+    BookItemComponent,
+    BookFormComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
